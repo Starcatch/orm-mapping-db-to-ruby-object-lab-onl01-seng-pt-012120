ec063da0d0f1648a7406d0d9191509736c7b5633
Timur Mishiev <timurmishiev@gmail.com>
2020-04-03 02:04:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
       end
   end
 
-  def self.first_x_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
       FROM students
@@ -84,16 +84,7 @@ class Student
       end.first
   end
 
-  def self.all_students_in_grade_x(grade)
-    sql = <<-SQL
-      SELECT * FROM students
-      WHERE grade = ?
-      SQL
-
-      DB[:conn].execute(sql, grade).map do |row|
-        self.new_from_db(row)
-      end
-  end
+  
 
   def save
     sql = <<-SQL
